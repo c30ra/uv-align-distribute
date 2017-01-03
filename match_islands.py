9fad6746819603aa9c16e5b338da4cbdfc7a3dca
Luca Carella <bkarelb@gmail.com>
2017-01-03 11:10:10 +0100

    embedding networkx

--- match_islands.py
+++ match_islands.py
@@ -25,6 +25,16 @@ from collections import defaultdict
 
 import operator
 import mathutils
+
+import os
+import sys
+# Add vendor directory to module search path
+parent_dir = os.path.abspath(os.path.dirname(__file__))
+vendor_dir = os.path.join(parent_dir, 'networkx')
+
+print(vendor_dir)
+sys.path.append(vendor_dir)
+
 import networkx
 
 
