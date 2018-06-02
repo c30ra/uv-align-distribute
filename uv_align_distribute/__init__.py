6ef92799aa9d5c86c658677cc511cb114d06a739
Luca Carella <bkarelb@gmail.com>
2018-06-02 15:11:12 +0200

    2nd try

--- uv_align_distribute/__init__.py
+++ uv_align_distribute/__init__.py
@@ -30,13 +30,13 @@ bl_info = {
 
 if "bpy" in locals():
     import imp
-    # imp.reload(align_operations)
-    # imp.reload(distribution_operations)
-    # imp.reload(ui)
-    # imp.reload(snap_islands)
-    # imp.reload(match_islands)
-    # # imp.reload(pack_islands)
-    # imp.reload(global_def)
+    imp.reload(align_operations)
+    imp.reload(distribution_operations)
+    imp.reload(ui)
+    imp.reload(snap_islands)
+    imp.reload(match_islands)
+    # imp.reload(pack_islands)
+    imp.reload(global_def)
     imp.reload(operator_manager)
 else:
     # from . import align_operations
