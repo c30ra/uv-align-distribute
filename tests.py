87d6ab8526f2843f042fd0906892cf9bbabe66d1
Luca Carella <c30ra@users.noreply.github.com>
2021-12-09 21:04:13 +0100

    added __main__ to tests.py

--- tests.py
+++ tests.py
@@ -2,34 +2,36 @@ import glob
 import subprocess
 import sys
 
-blenderExecutable = "blender"
 
-# allow override of blender executable (important for CI!)
-if len(sys.argv) > 1:
-    blenderExecutable = sys.argv[1]
+if __name__ == "__main__":
+    blenderExecutable = "blender"
 
-# iterate over each *.test.blend file in the "tests" directory
-# and open up blender with the .test.blend file
-# and the corresponding .test.py python script.
+    # allow override of blender executable (important for CI!)
+    if len(sys.argv) > 1:
+        blenderExecutable = sys.argv[1]
 
-print("start testing...")
-gl = glob.glob("./tests/**/*.test.blend")
-print("found %s tests: " % len(gl))
+    # iterate over each *.test.blend file in the "tests" directory
+    # and open up blender with the .test.blend file
+    # and the corresponding .test.py python script.
 
-errorCode = 0
-for file in gl:
-    # print("executing:", file)
-    cmd = [
-        blenderExecutable,
-        "--addons",
-        "uv_align_distribute",
-        "--factory-startup",
-        "-noaudio",
-        "-b",
-        file,
-        "--python",
-        file.replace(".blend", ".py"),
-    ]
-    errorCode |= subprocess.call(cmd, stdout=subprocess.DEVNULL)
+    print("start testing...")
+    gl = glob.glob("./tests/**/*.test.blend")
+    print("found %s tests: " % len(gl))
 
-exit(errorCode)
+    errorCode = 0
+    for file in gl:
+        # print("executing:", file)
+        cmd = [
+            blenderExecutable,
+            "--addons",
+            "uv_align_distribute",
+            "--factory-startup",
+            "-noaudio",
+            "-b",
+            file,
+            "--python",
+            file.replace(".blend", ".py"),
+        ]
+        errorCode |= subprocess.call(cmd, stdout=subprocess.DEVNULL)
+
+    exit(errorCode)
